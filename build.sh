0c1a3ea077f385ba21487b52cea1d6ab359f8c04
Park Ju Hyung <qkrwngud825@gmail.com>
2019-10-13 23:17:06 +0900

    Make the binary smaller
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-aarch64-linux-android-clang++ \
+API=29
+
+aarch64-linux-android${API}-clang++ \
     -Iinclude \
         main.cpp \
         system_property_set.cpp \
         systemproperties/*.cpp \
-    -s -Os -o resetprop \
-    -static-libstdc++ && ls -al resetprop
+    -s -Os -o resetprop && ls -al resetprop
+
+# We don't have to worry about inconsistent C++ library
+sed -i -e 's@libc++_shared.so@libc++.so\x00\x00\x00\x00\x00\x00\x00@g' resetprop
